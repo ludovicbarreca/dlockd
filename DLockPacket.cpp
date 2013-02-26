1973fa9564dc5b509ca2912200f83b7a8cb9de55
ludovicbarreca <ludogp2@gmail.com>
2013-02-27 03:43:43 +0900

    Modifying DLockPacket to be a real object

--- DLockPacket.cpp
+++ DLockPacket.cpp
@@ -1,6 +1,10 @@
 #include "DLockPacket.hpp"
 
 DLockPacket::DLockPacket(char opcode, QByteArray data) {
+	this->setOpcode(opcode);
+	this->setData(data);
+
+	// 2 nibbles = 1 byte :)
 	char flags = (version & 0xf) << 4 | (opcode & 0xf);
 	packet.append(flags);
 
@@ -9,15 +13,54 @@ DLockPacket::DLockPacket(char opcode, QByteArray data) {
 	packet.append(empty);
 	packet.append(empty);
 	packet.append(data);
+	this->updateCrc();
+}
 
-	// generating the checksum and replacing the empty crc16 field
+DLockPacket DLockPacket::setOpcode(char opcode, bool updateCrc) {
+	this->opcode = opcode;
+	char flags = (version & 0xf) << 4 | (opcode & 0xf);
+	packet.replace(0, 1, &flags, 1);
+
+	if (updateCrc == true) {
+		this->updateCrc();
+	}
+
+	return *this;
+}
+
+DLockPacket DLockPacket::setData(QByteArray data, bool updateCrc) {
+	this->data = data;
+	packet.replace(3, data.length(), data.constData());
+
+	if (updateCrc == true) {
+		this->updateCrc();
+	}
+
+	return *this;
+}
+
+char DLockPacket::getOpcode() {
+	return this->opcode;
+}
+
+QByteArray DLockPacket::getData() {
+	return this->data;
+}
+
+void DLockPacket::updateCrc() {
+	// setting back to 0 first
+	char empty = 0x00;
+	packet.replace(1, 1, &empty, 1);
+	packet.replace(2, 1, &empty, 1);
+	// then generating the crc
 	quint16 crc = qChecksum(packet.constData(), packet.length());
 	char low = crc;
 	char high = (crc >> 8) & 0xff;
+	// finally updating it
 	packet.replace(1, 1, &high, 1);
 	packet.replace(2, 1, &low, 1);
 }
 
 QByteArray DLockPacket::getPacket() {
-	return packet.toHex();
+	return packet;
 }
--- DLockPacket.hpp
+++ DLockPacket.hpp
@@ -3,16 +3,25 @@
 class DLockPacket {
 
 public:
-	char const PEER_ANNOUNCE = 0x0;
-	char const LOCK_REQUEST = 0x1;
-	char const LOCK_RESPONSE = 0x2;
-	char const LOCK_ANNOUNCE = 0x3;
+	enum Operation {
+		PEER_ANNOUNCE = 0x0,
+		LOCK_REQUEST  = 0x1,
+		LOCK_RESPONSE = 0x2,
+		LOCK_ANNOUNCE = 0x3
+	};
 
 	DLockPacket(char opcode, QByteArray data);
+	DLockPacket setOpcode(char opcode, bool updateCrc = false);
+	DLockPacket setData(QByteArray data, bool updateCrc = false);
+	char getOpcode();
+	QByteArray getData();
 	QByteArray getPacket();
+	void updateCrc();
 
 private:
 	char version = 0x0;
+	char opcode;
+	QByteArray data;
 	QByteArray packet;
 
 };
